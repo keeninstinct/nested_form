1ac78f35a9022fa077ced5f19911cc71b6d81bd3
William Richards <william@deap.ca>
2012-06-29 15:13:02 -0600

    Added some commented out code to help with future debugging

--- vendor/assets/javascripts/jquery_nested_form.js
+++ vendor/assets/javascripts/jquery_nested_form.js
@@ -1,4 +1,4 @@
-(function($) {
+jQuery(function($) {
   window.NestedFormEvents = function() {
     this.addFields = $.proxy(this.addFields, this);
     this.removeFields = $.proxy(this.removeFields, this);
@@ -14,14 +14,21 @@
       // Make the context correct by replacing new_<parents> with the generated ID
       // of each of the parent objects
       var context = ($(link).closest('.fields').find('input:first').attr('name') || '').replace(new RegExp('\[[a-z_]+\]$'), '');
+      
+      // The following commented out context is from the original nested_form repo - with the exception of the underscore in the regexp and the use of first-child
+      //var context = ($(link).closest('.fields').closestChild('input:first-child').attr('name') || '').replace(new RegExp('\[[a-z_]+\]$'), '');
 
       // context will be something like this for a brand new form:
       // project[tasks_attributes][new_1255929127459][assignments_attributes][new_1255929128105]
       // or for an edit form:
       // project[tasks_attributes][0][assignments_attributes][1]
       if (context) {
-		var parentNames = context.match(/[a-z_]+_attributes(?=\]\[(new_)?\d+\])/g) || [];
+		    var parentNames = context.match(/[a-z_]+_attributes(?=\]\[(new_)?\d+\])/g) || [];
         var parentIds   = context.match(/(new_)?[0-9]+/g) || [];
+        
+        // The following were the original parentNames and parentIds in nested_form from ryanb
+        //var parentNames = context.match(/[a-z_]+_attributes/g) || [];
+        //var parentIds   = context.match(/(new_)?[0-9]+/g) || [];
 
         for(var i = 0; i < parentNames.length; i++) {
           if(parentIds[i]) {
@@ -68,4 +75,34 @@
   window.nestedFormEvents = new NestedFormEvents();
   $('form a.add_nested_fields').live('click', nestedFormEvents.addFields);
   $('form a.remove_nested_fields').live('click', nestedFormEvents.removeFields);
+});
+
+// http://plugins.jquery.com/project/closestChild
+/*
+ * Copyright 2011, Tobias Lindig
+ *
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ *
+ */
+(function($) {
+  $.fn.closestChild = function(selector) {
+    // breadth first search for the first matched node
+    if (selector && selector != '') {
+      var queue = [];
+      queue.push(this);
+      while(queue.length > 0) {
+        var node = queue.shift();
+        var children = node.children();
+        for(var i = 0; i < children.length; ++i) {
+          var child = $(children[i]);
+          if (child.is(selector)) {
+            return child; //well, we found one
+          }
+          queue.push(child);
+        }
+      }
+    }
+    return $();//nothing found
+  };
 })(jQuery);
